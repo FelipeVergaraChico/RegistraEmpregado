962c519ce0349a37f3468ce5fb6bd0088ddfd6dc
Felipe Vergara Chico <felipe.vergara.chico@gmail.com>
2025-06-10 19:14:12 -0300

    Add: CRUD done, search queries done, get by departmentId done

--- src/controller/EmployeeController.js
+++ src/controller/EmployeeController.js
@@ -1,6 +1,8 @@
 const GetAllEmployees = require("../services/GetAllEmployees");
 const RegisterEmployeeService = require("../services/RegisterEmployeeService");
 const UpdateEmployeeService = require("../services/UpdateEmployeeService");
+const DeleteEmployeeService = require("../services/DeleteEmployeeService");
+const SearchEmployeeService = require("../services/SearchEmployeeService");
 
 module.exports = class EmployeeController {
   static async GetAllEmployees(req, res) {
@@ -11,6 +13,32 @@ module.exports = class EmployeeController {
       res
         .status(500)
         .json({ message: "Error fetching employees", error: error.message });
+    }
+  }
+  static async GetAllEmployeesByDepartmentId(req, res) {
+    const { departmentId } = req.params;
+
+    try {
+      const employees = await GetAllEmployees.GetAllEmployeesByDepartmentId(
+        departmentId
+      );
+      res.status(200).json(employees);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error fetching employees", error: error.message });
+    }
+  }
+  static async GetEmployeeById(req, res) {
+    const { id } = req.params;
+
+    try {
+      const employee = await GetAllEmployees.GetEmployeeById(id);
+      res.status(200).json(employee);
+    } catch (error) {
+      res
+        .status(404)
+        .json({ message: "Empregado não encontrado", error: error.message });
     }
   }
   static async Register(req, res) {
@@ -56,6 +84,34 @@ module.exports = class EmployeeController {
           message: "Erro ao atualizar o empregado",
           error: error.message,
         });
+    }
+  }
+  static async DeleteEmployee(req, res) {
+    const { id } = req.params;
+
+    try {
+      const response = await DeleteEmployeeService.DeleteEmployee(id, req, res);
+      res.status(200).json(response);
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: "Erro ao deletar o empregado", error: error.message });
+    }
+  }
+  static async SearchEmployee(req, res) {
+    const { searchTerm } = req.query;
+
+    try {
+      const employees = await SearchEmployeeService.SearchEmployee(
+        searchTerm,
+        req,
+        res
+      );
+      res.status(200).json(employees);
+    } catch (error) {
+      res
+        .status(404)
+        .json({ message: "Nenhum empregado encontrado", error: error.message });
     }
   }
 };
--- src/routes/EmployeeRoutes.js
+++ src/routes/EmployeeRoutes.js
@@ -2,8 +2,13 @@ const router = require("express").Router();
 
 const EmployeeController = require("../controller/EmployeeController");
 
+router.get("/search", EmployeeController.SearchEmployee);
+
 router.get("/", EmployeeController.GetAllEmployees);
+router.get("/:id", EmployeeController.GetEmployeeById);
+router.get("/department/:departmentId", EmployeeController.GetAllEmployeesByDepartmentId);
 router.post("/register", EmployeeController.Register);
 router.patch("/update/:id", EmployeeController.UpdateEmployee);
+router.delete("/delete/:id", EmployeeController.DeleteEmployee);
 
 module.exports = router;
